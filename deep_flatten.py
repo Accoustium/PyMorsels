9514d9a0b4e27dcaa031a0cfa7e083186a19bc3d
tim.pogue <tim.pogue@cybera.net>
2020-03-04 07:48:19 -0600

    fixing deep_flatten

--- deep_flatten.py
+++ deep_flatten.py
@@ -1,9 +1,16 @@
-def deep_flatten(iterable: list):
-    output = []
-    for item in iterable:
-        if type(item) == int or type(item) == str:
-            output.append(item)
-        else:
-            output.extend(deep_flatten(item))
+class deep_flatten:
+    def __init__(self, iterable: list):
+        self.iterable = iter(iterable)
 
-    return output
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return next(iter(self.flatten(self.iterable)))
+
+    def flatten(self, iterable):
+        for item in iterable:
+            if isinstance(item, (str, int)):
+                yield item
+            else:
+                yield from self.flatten(item)

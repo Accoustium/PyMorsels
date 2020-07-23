3f646cd7f69fbcdfd62375d09c662ea4e08eacfb
tim.pogue <tim.pogue@cybera.net>
2020-07-23 16:05:46 -0500

    complete suppress request

--- suppress.py
+++ suppress.py
@@ -1,12 +1,30 @@
-import sys
 from contextlib import ContextDecorator
 
 
 class suppress(ContextDecorator):
     def __init__(self, *exceptions):
         self.exceptions = exceptions
-        try:
-            pass
-        except self.exceptions:
-            self.exception = sys.exc_info()[1]
-            self.traceback = sys.exc_info()[2]
+        self.exception = None
+        self.traceback = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type:
+            if (
+                exc_type not in self.exceptions and
+                set(exc_type.__bases__).intersection(
+                    set(
+                        [self.exceptions]
+                        if type(self.exceptions) != tuple
+                        else self.exceptions
+                    )
+                ) == set()
+            ):
+                raise exc_val
+
+            self.exception = exc_val
+            self.traceback = exc_tb
+
+        return self

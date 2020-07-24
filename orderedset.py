59c320a1aac5f7c95fa88d3b3c9d3359cdff633f
tim.pogue <tim.pogue@cybera.net>
2020-07-24 14:07:52 -0500

    remove old misspelled file, and finalize orderedset with all extra tests passing.

--- orderedset.py
+++ orderedset.py
@@ -1,21 +1,42 @@
 class OrderedSet:
     def __init__(self, list_of_values):
         self.values = list()
+        self.set_values = set()
+        self.gen = None
         for item in list_of_values:
             self.add(item)
 
+    def __str__(self):
+        return self.values
+
+    def __getitem__(self, item):
+        return self.values[item]
+
+    def __iter__(self):
+        if self.gen is None:
+            self.gen = (x for x in self.values)
+
+        return self.gen
+
+    def __len__(self):
+        return len(self.values)
+
     def __eq__(self, other):
-        if isinstance(other, OrderedSet):
-            return set(self.values) == set(other.values)
+        if isinstance(other, OrderedSet) is True:
+            return self.values == other.values
         else:
             return set(self.values) == other
 
+    def __contains__(self, item):
+        return item in self.set_values
+
     def add(self, obj):
-        if object not in self.values:
+        if obj not in self.set_values:
             self.values.append(obj)
+            self.set_values.add(obj)
 
     def discard(self, obj):
         if obj in self.values:
+            self.set_values.remove(obj)
             idx = self.values.index(obj)
-            print(idx)
             trash = self.values.pop(idx)

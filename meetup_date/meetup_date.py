a2e21ee3ce96e5ab1e5599b5162eb6d330cae31e
Accoustium <t.pogue.python@gmail.com>
2021-08-27 08:05:56 -0500

    make work for standard tests

--- meetup_date/meetup_date.py
+++ meetup_date/meetup_date.py
@@ -1,32 +1,49 @@
-import enum
 import datetime
+from dataclasses import dataclass
 from calendar import Calendar
 
 
-class Weekday(enum.Enum):
-    MONDAY = 0
-    TUESDAY = 1
-    WEDNESDAY = 2
-    THURSDAY = 3
-    FRIDAY = 4
-    SATURDAY = 5
-    SUNDAY = 6
+@dataclass(frozen=True)
+class Weekday:
+    MONDAY: int = 0
+    TUESDAY: int = 1
+    WEDNESDAY: int = 2
+    THURSDAY: int = 3
+    FRIDAY: int = 4
+    SATURDAY: int = 5
+    SUNDAY: int = 6
 
 
 class meetup_date:
-    def __init__(self, year: int, month: int, nth: int = 4, weekday: Weekday = Weekday.THURSDAY):
-        if isinstance(weekday, int):
-            weekday = Weekday(weekday)
-
+    def __init__(self, year: int, month: int, nth: int = 4, weekday: int = Weekday.THURSDAY):
         self.cal: Calendar = Calendar()
         self.month: list = self.cal.monthdatescalendar(year, month)
         if nth > 0:
-            self.meetup: datetime.date = list(filter(lambda x: x >= datetime.date(year, month, 1), map(lambda x: x[nth - 1], self.month)))[weekday.value]
+            self.meetup: datetime.date = list(
+                filter(
+                    lambda x: x >= datetime.date(year, month, 1),
+                    map(
+                        lambda x: x[nth - 1],
+                        self.month
+                    )
+                )
+            )[weekday]
         else:
-            self.meetup: datetime.date = list(filter(lambda x: x >= datetime.date(year, month, 1), map(lambda x: x[nth], self.month)))[weekday.value]
+            self.meetup: datetime.date = list(
+                filter(
+                    lambda x: x >= datetime.date(year, month, 1),
+                    map(
+                        lambda x: x[nth],
+                        self.month
+                    )
+                )
+            )[weekday]
 
     def __repr__(self):
         return repr(self.meetup)
 
     def __str__(self):
         return self.meetup.strftime('%Y-%m-%d')
+
+    def __eq__(self, other):
+        return self.meetup == other

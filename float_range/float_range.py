f97d4268fa84f2c8a3fda10b40401f4ad7e24699
Accoustium <t.pogue.python@gmail.com>
2021-09-14 16:50:04 -0500

    Pass all but equality

--- float_range/float_range.py
+++ float_range/float_range.py
@@ -1,6 +1,6 @@
 class float_range:
     def __init__(self, start: float, stop: float = None, step: float = 1.0):
-        if step == 0:
+        if step == 0.0:
             raise ValueError
 
         self.step = float(step)
@@ -13,8 +13,37 @@ class float_range:
             self.start = float(start)
             self.stop = float(stop)
 
+    def __iter__(self):
+        return FloatRangeIterator(self)
+
     def __repr__(self):
         return f"float_range({self.start}, {self.stop}, {self.step})"
+
+    def __reversed__(self):
+        return iter(float_range(start=self.__find_last(), stop=self.start - self.step, step=self.step * -1))
+
+    def __eq__(self, other):
+        if isinstance(other, float_range):
+            return self.start == other.start and self.__find_last() == other.__find_last() and self.step == other.step
+
+        return all([self.start == other, self.stop == other, self.step == other])
+
+    def __len__(self):
+        if self.step > 0 and self.stop > self.start:
+            return int(round((self.stop - self.start) / self.step, 0))
+        elif self.step < 0 and self.start > self.stop:
+            return int(round((self.start - self.stop) / (self.step * -1), 0))
+
+        return 0
+
+    def __find_last(self):
+        return (len(self) - 1) * self.step + self.start
+
+
+class FloatRangeIterator:
+    def __init__(self, range_float: float_range):
+        self.float_range = range_float
+        self.__curr = None
 
     def __iter__(self):
         return self
@@ -24,26 +53,11 @@ class float_range:
             self.__curr = self.__step()
         return next(self.__curr)
 
-    def __reversed__(self):
-        new = list(self)
-        return float_range(start=new[-1], stop=new[0] - self.step, step=self.step * -1)
-
-    def __eq__(self, other):
-        if isinstance(other, float_range) or isinstance(other, range):
-            return self.start == other.start and self.stop == other.stop and self.step == other.step
-
-        return False
-
-    def __len__(self):
-        if self.step > 0 and self.stop > self.start:
-            return int(round((self.stop - self.start) / self.step, 0))
-        elif self.step < 0 and self.start > self.stop:
-            return int(round((self.start - self.stop) / (self.step * -1), 0))
-        else:
-            return 0
-
     def __step(self):
-        v = self.start
-        while (v < self.stop and self.step > 0) or (v > self.stop and self.step < 0):
+        v = self.float_range.start
+        while (
+                (v < self.float_range.stop and self.float_range.step > 0) or
+                (v > self.float_range.stop and self.float_range.step < 0)
+        ):
             yield v
-            v += self.step
+            v += self.float_range.step
